2dce4471bd9969463e0a986c569335f1b44a4667
= <=>
2018-09-23 10:13:07 +0700

    su dung toan tu

---
+++ bai_tap.js
@@ -0,0 +1,6 @@
+    var height;
+    var width;
+    var height = parseInt(prompt("Enter the height"));
+    var width =parseInt(prompt("Endter the width"));
+    var area = height * width;
+    document.write("the area is:" + area);
